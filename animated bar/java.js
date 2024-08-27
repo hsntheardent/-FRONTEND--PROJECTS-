c4f85b6a71a12f4d9b0e1ab5ecf9500d237cd192
Ehtesham <168669920+hsntheardent@users.noreply.github.com>
2024-08-27 19:06:35 +0500

    Add files via upload

---
+++ animated bar/java.js
@@ -0,0 +1,12 @@
+let searchIcon = document.querySelector('.search-icon')
+let searchBar = document.querySelector('.search-bar')
+
+searchIcon.addEventListener("click", ()=>{
+    searchBar.classList.toggle('inactive')
+})
+
+
+
+
+
+
